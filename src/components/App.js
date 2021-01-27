e915ce0726eec5ef8f54bf2c575ecd62c83d5978
Neil <me@neil.pro>
2021-01-26 21:01:03 -0300

    Actions, reducers, middleware, initial app data and Dashboard component
    Yep, that was too large. I'll stop doing that, don't worry.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
-function App() {
-  return (
-    <div>
-      Starter code
-    </div>
-  );
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
+
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+
+  render () {
+    return (
+      <div>
+        {this.props.loading ? null : <Dashboard />}
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps ( { authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App);
